d72fac7e640e665a5d167e483435880983bfd9ca
Nurtic-Vibe <nurtic-vibe@grmml.eu>
2015-09-11 13:25:28 +0200

    tried to fix exabgp

---
+++ srv/exabgp/res-watchdog.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+source `dirname`/watchdog.conf
+
+DIG=dig
+TARGETS=( "res" )
+ZONE=dn42
+ROUTE='172.23.0.48/28'
+NEXTHOP="$OWN_IP" # own ip
+
+INTERVAL=60
+
+VALIDATE_KEYWORD='NS.*zone-servers\.dn42\.'
+
+###########################
+
+RUN_STATE=0
+
+sleep 5
+
+check_targets() {
+        for item in "${TARGETS[@]}"; do
+                ${DIG} @${item} ${ZONE} ANY | egrep -q "${VALIDATE_KEYWORD}" ||
+                return 1
+        done
+
+	return 0
+}
+
+while [ 1 ]; do
+        if [ ${RUN_STATE} -eq 0 ]; then
+                check_targets && {
+                        RUN_STATE=1
+                        echo "announce route ${ROUTE} next-hop ${NEXTHOP}"
+                }
+        else
+            	check_targets || {
+                        RUN_STATE=0
+                        echo "withdraw route ${ROUTE} next-hop ${NEXTHOP}"
+                }
+        fi
+
+	sleep ${INTERVAL}
+
+done
+
+exit 0
---
+++ srv/exabgp/tier0-watchdog.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+source `dirname`/watchdog.conf
+
+DIG=dig
+TARGETS=( "tier0" )
+ZONE=dn42
+ROUTE="$TIER0/32"
+NEXTHOP="$OWN_IP" # own ip
+
+INTERVAL=60
+
+VALIDATE_KEYWORD='NS.*zone-servers\.dn42\.'
+
+###########################
+
+RUN_STATE=0
+
+sleep 5
+
+check_targets() {
+        for item in "${TARGETS[@]}"; do
+                ${DIG} @${item} ${ZONE} ANY | egrep -q "${VALIDATE_KEYWORD}" ||
+                return 1
+        done
+
+	return 0
+}
+
+while [ 1 ]; do
+        if [ ${RUN_STATE} -eq 0 ]; then
+                check_targets && {
+                        RUN_STATE=1
+			echo "announce route ${ROUTE} next-hop ${NEXTHOP}"
+                }
+        else
+            	check_targets || {
+                        RUN_STATE=0
+			echo "withdraw route ${ROUTE} next-hop ${NEXTHOP}"
+                }
+        fi
+
+	sleep ${INTERVAL}
+
+done
+
+exit 0
+
